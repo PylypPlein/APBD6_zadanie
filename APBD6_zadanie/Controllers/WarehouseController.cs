3d1ac19ba5aafa43ced0ef69ec959c0d680c8f0b
s24675 <s24675@pjwstk.edu.pl>
2024-05-09 21:43:35 +0200

    Zaimplementowano WarehouseController.cs, WarehouseController.cs,WarehouseService.sc

---
+++ APBD6_zadanie/Controllers/WarehouseController.cs
@@ -0,0 +1,37 @@
+﻿using APBD6_zadanie.Exceptions;
+using APBD6_zadanie.Services;
+
+namespace APBD6_zadanie.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class WarehouseController : ControllerBase
+{
+    private readonly IWarehouseService _warehouseService;
+    public WarehouseController(IWarehouseService warehouseService)
+    {
+        _warehouseService = warehouseService;
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> RegisterProductInWarehouse([FromBody] RegisterProductInWarehouseRequestDTO dto)
+    {
+        try
+        {
+            var idProductWarehouse = await _warehouseService.RegisterProductInWarehouseAsync(dto);
+            return Ok(idProductWarehouse);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (ConflictException e)
+        {
+            return Conflict(e.Message);
+        }
+    }
+}
